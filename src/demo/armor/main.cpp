8a73f033a962e8d05c9596dad744b2d17420aa3c
Liu Zhongqi <3130676972@qq.com>
2021-09-16 19:07:00 +0800

    更新demo写法

--- src/demo/armor/main.cpp
+++ src/demo/armor/main.cpp
@@ -1,71 +1,83 @@
 #include <iostream>
 
+#include "app/app.hpp"
 #include "armor_detector.hpp"
 #include "hik_camera.hpp"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/spdlog.h"
+#include "opencv2/videoio.hpp"
 
 namespace {
-const std::string source_path = "../../../../image/2.avi";
-const std::string output_path = "../../../../image/test_detect.avi";
-const std::string params_path =
-    "../../../../src/demo/armor/params_test_blue.json";
+
+const std::string kSOURCE_PATH = "../../../../../../1.avi";
+const std::string kOUTPUT_PATH = "../../../../image/test_detect.avi";
+const std::string kPARAM_PATH = "../../../../runtime/RMUL2021_Armor.json ";
 
 }  // namespace
+
+class ArmorDemo : private App {
+ private:
+  cv::VideoCapture cap_;
+  cv::VideoWriter writer_;
+  ArmorDetector detector_;
+  cv::Mat frame_;
+
+ public:
+  ArmorDemo(const std::string& log_path) : App(log_path) {
+    SPDLOG_WARN("***** Setting Up Armor Detecting Demo. *****");
+
+    /* 初始化 */
+    cap_.open(kSOURCE_PATH);
+
+    const int codec = cap_.get(cv::CAP_PROP_FOURCC);
+    const double fps = cap_.get(cv::CAP_PROP_FPS);
+    cv::Size f_size(cap_.get(cv::CAP_PROP_FRAME_WIDTH),
+                    cap_.get(cv::CAP_PROP_FRAME_HEIGHT));
+
+    writer_.open(kOUTPUT_PATH, codec, fps, f_size);
+
+    if (!cap_.isOpened()) {
+      SPDLOG_ERROR("Can't Open Video {}", kSOURCE_PATH);
+    }
+    if (!writer_.isOpened()) {
+      SPDLOG_ERROR("Can't Write Video {}", kOUTPUT_PATH);
+    }
+
+    detector_.LoadParams(kPARAM_PATH);
+    detector_.SetEnemyTeam((game::Team::kBLUE));
+  }
+
+  ~ArmorDemo() { SPDLOG_WARN("***** Shuted Down Armor Detecting Demo. *****"); }
+
+  /* Demo Running */
+  void Run() {
+    SPDLOG_WARN("***** Running Armor Detecting Demo. *****");
+
+    const int delay = static_cast<int>(1000. / cap_.get(cv::CAP_PROP_FPS));
+
+    while (1) {
+      cap_.read(frame_);
+      if (frame_.empty()) continue;
+
+      auto armors = detector_.Detect(frame_);
+      detector_.VisualizeResult(frame_, 10);
+      writer_.write(frame_);
+
+      cv::imshow("video", frame_);
+      if (' ' == cv::waitKey(delay)) {
+        cv::waitKey(0);
+      }
+      if ('q' == cv::waitKey(delay)) {
+        exit(0);
+      }
+    }
+  }
+};
 
 int main(int argc, char const* argv[]) {
   (void)argc;
   (void)argv;
 
-  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-      "logs/demo_armor.log", true);
+  ArmorDemo demo("logs/armor_demo.log");
+  demo.Run();
 
-  spdlog::sinks_init_list sink_list = {console_sink, file_sink};
-
-  spdlog::set_default_logger(
-      std::make_shared<spdlog::logger>("default", sink_list));
-
-#if (SPDLOG_ACTIVE_LEVEL == SPDLOG_LEVEL_DEBUG)
-  spdlog::flush_on(spdlog::level::debug);
-  spdlog::set_level(spdlog::level::debug);
-#elif (SPDLOG_ACTIVE_LEVEL == SPDLOG_LEVEL_INFO)
-  spdlog::flush_on(spdlog::level::info);
-  spdlog::set_level(spdlog::level::info);
-#endif
-
-  SPDLOG_WARN("***** Running Armor Detecting Demo. *****");
-
-  cv::VideoCapture cap(source_path);
-  if (!cap.isOpened()) {
-    SPDLOG_ERROR("Can't open {}.", source_path);
-    return -1;
-  }
-
-  cv::Size f_size(cap.get(cv::CAP_PROP_FRAME_WIDTH),
-                  cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-  const double fps = cap.get(cv::CAP_PROP_FPS);
-  const int delay = static_cast<int>(1000. / fps);
-  const int codec = cap.get(cv::CAP_PROP_FOURCC);
-
-  cv::VideoWriter writer(output_path, codec, fps, f_size);
-  if (!writer.isOpened()) {
-    SPDLOG_ERROR("Can't write to {}.", output_path);
-    return -1;
-  }
-
-  ArmorDetector detector(params_path, game::Team::kBLUE);
-
-  cv::Mat frame;
-  while (cap.read(frame)) {
-    detector.Detect(frame);
-    detector.VisualizeResult(frame, 2);
-
-    cv::imshow("vedio", frame);
-    if (cv::waitKey(delay) == 'q') break;
-
-    writer.write(frame);
-  }
   return 0;
 }
