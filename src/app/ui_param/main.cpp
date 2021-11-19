14c0c33b7def158d641ef7fd8b34935cd09e4647
define9 <924753116@qq.com>
2021-11-19 20:22:53 +0800

    修改

--- src/app/ui_param/main.cpp
+++ src/app/ui_param/main.cpp
@@ -5,8 +5,8 @@
 #include "armor_param.hpp"
 
 namespace {
-  const std::string params_path = "../../../../runtime/RMUL2021_Armor.json";
-  const std::string window_name = "ui_setting";
+  const std::string kPARAM = "../../../../runtime/RMUL2021_Armor.json";
+  const std::string kWINDOW = "ui_setting";
 }
 
 class UIParam : private App {
@@ -22,7 +22,7 @@ class UIParam : private App {
     /* 初始化设备 */
     cam_.Open(0);
     cam_.Setup(640, 480);
-    armor_param_.Read(params_path);
+    armor_param_.Read(kPARAM);
   }
 
   ~UIParam() {
@@ -34,48 +34,49 @@ class UIParam : private App {
   /* 运行的主程序 */
   void Run() override {
     SPDLOG_WARN("start_UiSetting");
+    cv::namedWindow(kWINDOW, 1);
 
-    cv::createTrackbar("binary_th", window_name, &armor_param_.param_int.binary_th, 255);
-    cv::createTrackbar("contour_size_low_th", window_name,
+    cv::createTrackbar("binary_th", kWINDOW, &armor_param_.param_int.binary_th, 255, 0);
+    cv::createTrackbar("contour_size_low_th", kWINDOW,
                        &armor_param_.param_int.contour_size_low_th, 10240);
-    cv::createTrackbar("contour_area_low_th", window_name,
+    cv::createTrackbar("contour_area_low_th", kWINDOW,
                        &armor_param_.param_int.contour_area_low_th, 255);
-    cv::createTrackbar("contour_area_high_th", window_name,
+    cv::createTrackbar("contour_area_high_th", kWINDOW,
                        &armor_param_.param_int.contour_area_high_th, 255);
-    cv::createTrackbar("bar_area_low_th", window_name, &armor_param_.param_int.bar_area_low_th,
+    cv::createTrackbar("bar_area_low_th", kWINDOW, &armor_param_.param_int.bar_area_low_th,
                        255);
-    cv::createTrackbar("bar_area_high_th", window_name,
+    cv::createTrackbar("bar_area_high_th", kWINDOW,
                        &armor_param_.param_int.bar_area_high_th, 255);
-    cv::createTrackbar("angle_high_th", window_name, &armor_param_.param_int.angle_high_th,
+    cv::createTrackbar("angle_high_th", kWINDOW, &armor_param_.param_int.angle_high_th,
                        255);
-    cv::createTrackbar("aspect_ratio_low_th", window_name,
+    cv::createTrackbar("aspect_ratio_low_th", kWINDOW,
                        &armor_param_.param_int.aspect_ratio_low_th, 255);
-    cv::createTrackbar("aspect_ratio_high_th", window_name,
+    cv::createTrackbar("aspect_ratio_high_th", kWINDOW,
                        &armor_param_.param_int.aspect_ratio_high_th, 255);
-    cv::createTrackbar("angle_diff_th", window_name, &armor_param_.param_int.angle_diff_th,
+    cv::createTrackbar("angle_diff_th", kWINDOW, &armor_param_.param_int.angle_diff_th,
                        255);
-    cv::createTrackbar("length_diff_th", window_name, &armor_param_.param_int.length_diff_th,
+    cv::createTrackbar("length_diff_th", kWINDOW, &armor_param_.param_int.length_diff_th,
                        255);
-    cv::createTrackbar("height_diff_th", window_name, &armor_param_.param_int.height_diff_th,
+    cv::createTrackbar("height_diff_th", kWINDOW, &armor_param_.param_int.height_diff_th,
                        255);
-    cv::createTrackbar("area_diff_th", window_name, &armor_param_.param_int.area_diff_th, 255);
-    cv::createTrackbar("center_dist_low_th", window_name,
+    cv::createTrackbar("area_diff_th", kWINDOW, &armor_param_.param_int.area_diff_th, 255);
+    cv::createTrackbar("center_dist_low_th", kWINDOW,
                        &armor_param_.param_int.center_dist_low_th, 255);
-    cv::createTrackbar("center_dist_high_th", window_name,
+    cv::createTrackbar("center_dist_high_th", kWINDOW,
                        &armor_param_.param_int.center_dist_high_th, 255);
 
     while (true) {
       cv::Mat frame = cam_.GetFrame();
-      if (frame.empty()) continue;
+      if (frame.empty())  continue;
 
       detector_.params_ = armor_param_.transform2Double();
       detector_.Detect(frame);
       detector_.VisualizeResult(frame, 10);
-      cv::imshow(window_name, frame);
+      cv::imshow(kWINDOW, frame);
       cv::imshow("img", frame);
       char key = cv::waitKey(10);
       if (key == 's') {
-        armor_param_.Write(params_path);
+        armor_param_.Write(kPARAM);
       } else if (key == 'q' || key == 27) {
         return;
       }
