bca522c13381c92a744ca114cdca738e2b480814
Liu Zhongqi <3130676972@qq.com>
2022-01-28 01:36:44 +0800

    解决由于GTest更新而带来的ASSERT_EQ不兼容Error

--- tests/vision/armor.cpp
+++ tests/vision/armor.cpp
@@ -4,24 +4,22 @@
 #include "light_bar.hpp"
 #include "opencv2/opencv.hpp"
 
-const cv::RotatedRect left_rect(cv::Point2f(1., 3.), cv::Size2f(1., 3.), 5.);
-
-const cv::RotatedRect right_rect(cv::Point2f(3., 1.), cv::Size2f(1., 3.), 7.);
-
-const LightBar left_bar(left_rect);
-const LightBar right_bar(right_rect);
-
-const game::Model model = game::Model::kHERO;
-
 TEST(TestVision, TestArmor) {
+  const cv::RotatedRect left_rect(cv::Point2f(1., 3.), cv::Size2f(1., 3.), 5.);
+  const cv::RotatedRect right_rect(cv::Point2f(3., 1.), cv::Size2f(1., 3.), 7.);
+  const LightBar left_bar(left_rect);
+  const LightBar right_bar(right_rect);
+
+  const game::Model model = game::Model::kHERO;
+
   Armor armor(left_bar, right_bar);
 
   ASSERT_FLOAT_EQ(armor.ImageCenter().x, 2.);
   ASSERT_FLOAT_EQ(armor.ImageCenter().y, 2.);
   ASSERT_FLOAT_EQ(armor.ImageAngle(), 6.);
 
-  ASSERT_EQ(armor.GetModel(), game::Model::kUNKNOWN);
+  ASSERT_TRUE(armor.GetModel() == game::Model::kUNKNOWN);
 
   armor.SetModel(model);
-  ASSERT_EQ(armor.GetModel(), model);
+  ASSERT_TRUE(armor.GetModel() == model);
 }
--- tests/vision/armor_classifier.cpp
+++ tests/vision/armor_classifier.cpp
@@ -17,14 +17,14 @@ game::Model Classify(const std::string& path) {
 }
 
 TEST(TestVision, TestArmorClassifier) {
-  ASSERT_EQ(Classify("../../../image/test_classifier_0.png"),
-            game::Model::kINFANTRY);
-  ASSERT_EQ(Classify("../../../image/test_classifier_1.png"),
-            game::Model::kENGINEER);
-  ASSERT_EQ(Classify("../../../image/test_classifier_2.png"),
-            game::Model::kHERO);
-  ASSERT_EQ(Classify("../../../image/test_classifier_3.png"),
-            game::Model::kHERO);
+  ASSERT_TRUE(Classify("../../../image/test_classifier_0.png") ==
+              game::Model::kINFANTRY);
+  ASSERT_TRUE(Classify("../../../image/test_classifier_1.png") ==
+              game::Model::kENGINEER);
+  ASSERT_TRUE(Classify("../../../image/test_classifier_2.png") ==
+              game::Model::kHERO);
+  ASSERT_TRUE(Classify("../../../image/test_classifier_3.png") ==
+              game::Model::kHERO);
 }
 
 TEST(TestVision, TestArmorClassifierInput) {
