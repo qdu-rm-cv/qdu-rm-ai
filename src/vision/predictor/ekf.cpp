1207b3538fc16139b4c257e737efdd7cf64489e5
C12H22O11 <3130676972@qq.com>
2022-03-22 10:22:16 +0800

    完成EKF并修改相关CMake

--- src/vision/predictor/ekf.cpp
+++ src/vision/predictor/ekf.cpp
@@ -1,28 +1,93 @@
 #include "ekf.hpp"
 
 #include <cmath>
+#include <vector>
 
 #include "spdlog/spdlog.h"
 
-void EKF::InnerInit(const Vec5d& Xe) {
-  this->Xe = Xe;
-  cv::setIdentity(P);
-  cv::setIdentity(Q);
-  cv::setIdentity(R);
+std::vector<ceres::Jet<double, 5>> LinerModel(
+    const std::vector<ceres::Jet<double, 5>> x0, double delta_t) {
+  std::vector<ceres::Jet<double, 5>> x1(5);
+  x1[0] = x0[0] + delta_t * x0[1];  // 0.1
+  x1[1] = x0[1];                    // 100
+  x1[2] = x0[2] + delta_t * x0[3];  // 0.1
+  x1[3] = x0[3];                    // 100
+  x1[4] = x0[4];                    // 0.01
+  SPDLOG_WARN("LinerModel");
+  return x1;
 }
+
+std::vector<ceres::Jet<double, 5>> ConvertFromCoordToEuler(
+    const std::vector<ceres::Jet<double, 5>> coord) {
+  std::vector<ceres::Jet<double, 5>> result(3);
+  // pitch
+  result[0] = ceres::atan2(
+      coord[4], ceres::sqrt(coord[0] * coord[0] + coord[2] * coord[2]));
+  // yaw
+  result[1] = ceres::atan2(coord[2], coord[0]);
+  // distance
+  result[2] = ceres::sqrt(coord[0] * coord[0] + coord[2] * coord[2] +
+                          coord[4] * coord[4]);
+  return result;
+}
+
+void EKF::InnerInit(const Matx51d& Xe) {
+  cv::cv2eigen(Xe, EXe);
+  this->EXe = EXe;
+  EP = EMatx55d::Identity();
+  EQ = EMatx55d::Identity();
+  ER = EMatx33d::Identity();
+}
+
 EKF::EKF() { SPDLOG_TRACE("Constructed."); }
 
-EKF::EKF(const Vec5d& Xe = Vec5d::zeros()) { InnerInit(Xe); }
+EKF::EKF(const Matx51d& Xe = Matx51d::zeros()) { InnerInit(Xe); }
 
 EKF::~EKF() { SPDLOG_TRACE("Destruted."); }
 
 void EKF::Init(const std::vector<double>& vec) {
   if (method_ == Method::kUNKNOWN) method_ = Method::kEKF;
-  InnerInit(Vec5d(vec[0], vec[1], vec[2], vec[3], vec[4]));
+  InnerInit(Matx51d(vec[0], vec[1], vec[2], vec[3], vec[4]));
 }
 
 const cv::Mat& EKF::Predict(const cv::Mat& measurements) {
-  return measurements;
+  std::vector<ceres::Jet<double, 5>> EXe_auto_jet(5), EXp_auto_jet(5);
+
+  for (int i = 0; i < 5; i++) {
+    EXe_auto_jet[i].a = EXe[i];
+    EXe_auto_jet[i].v[i] = 1;
+  }
+  EXp_auto_jet = LinerModel(EXe_auto_jet, measurements.at<double>(0));
+  for (int i = 0; i < 5; i++) {
+    EXp[i] = EXp_auto_jet[i].a;
+    EF.block(i, 0, 1, 5) = EXp_auto_jet[i].v.transpose();
+  }
+
+  EP = EF * EP * EF.transpose() + EQ;
+  cv::eigen2cv(EXp, Xp);
+  SPDLOG_WARN("Predicted");
+
+  return Xp;
 }
 
-// TODO : 完成EKF
+const cv::Mat& EKF::Update(const cv::Mat& measurements) {
+  std::vector<ceres::Jet<double, 5>> EXp_auto_jet(5), EYp_auto_jet(3);
+
+  EMatx31d Y;
+  cv::cv2eigen(Matx31d(measurements), Y);
+
+  for (int i = 0; i < 5; i++) {
+    EXp_auto_jet[i].a = EXp[i];
+    EXp_auto_jet[i].v[i] = 1;
+  }
+  EYp_auto_jet = ConvertFromCoordToEuler(EXp_auto_jet);
+  for (int i = 0; i < 3; i++) {
+    EYp[i] = EYp_auto_jet[i].a;
+    EH.block(i, 0, 1, 5) = EYp_auto_jet[i].v.transpose();
+  }
+  EK = EP * EH.transpose() * (EH * EP * EH.transpose() + ER).inverse();
+  EXe = EXp + EK * (Y - EYp);
+  EP = (EMatx55d::Identity() - EK * EH) * EP;
+  cv::eigen2cv(EXe, Xe);
+  return Xe;
+}
--- src/vision/predictor/ekf.hpp
+++ src/vision/predictor/ekf.hpp
@@ -1,34 +1,59 @@
 #pragma once
+
+#include <Eigen/Dense>
+
+#include "ceres/jet.h"
 #include "filter.hpp"
+#include "opencv2/core/eigen.hpp"
 
 class EKF : public Filter {
  public:
-  typedef cv::Matx33d Matx33d;
-  typedef cv::Matx<double, 3, 5> Matx35d;
-  typedef cv::Matx<double, 5, 3> Matx53d;
-  typedef cv::Matx<double, 5, 5> Matx55d;
-  typedef cv::Vec3d Vec3d;
-  typedef cv::Matx<double, 5, 1> Vec5d;
+  using EMatx55d = Eigen::Matrix<double, 5, 5>;
+  using EMatx35d = Eigen::Matrix<double, 3, 5>;
+  using EMatx53d = Eigen::Matrix<double, 5, 3>;
+  using EMatx33d = Eigen::Matrix<double, 3, 3>;
+  using EMatx51d = Eigen::Matrix<double, 5, 1>;
+  using EMatx31d = Eigen::Matrix<double, 3, 1>;
+
+  using Matx33d = cv::Matx33d;
+  using Matx35d = cv::Matx<double, 3, 5>;
+  using Matx53d = cv::Matx<double, 5, 3>;
+  using Matx55d = cv::Matx<double, 5, 5>;
+  using Matx31d = cv::Matx<double, 3, 1>;
+  using Matx51d = cv::Matx<double, 5, 1>;
 
  private:
-  Vec5d Xe;   // Vec5d state_pre_;                  /* 估计状态变量 */
-  Vec5d Xp;   // Vec5d state_next_;                 /* 预测状态变量 */
-  Matx55d F;  // Matx55d predict_mat_;              /* 预测雅克比 */
-  Matx35d H;  // Matx35d measurement_mat_;          /* 观测雅克比 */
-  Matx55d P;  // Matx55d state_cov_;                /* 状态协方差 */
-  Matx55d Q;  // Matx55d process_noi_cov_mat_;      /* 预测过程协方差 */
-  Matx33d R;  // Matx33d measurement_noi_cov_mat_;  /* 观测过程协方差 */
-  Matx35d K;  // Matx35d kalman_gain_;              /* 卡尔曼增益 */
-  Vec3d Yp;   // Vec3d predict_obs_;                /* 预测观测量 */
+  cv::Mat Xe;  // Matx51d state_pre_;             /* 估计状态变量 */
+  cv::Mat Xp;  // Matx51d state_next_;            /* 预测状态变量 */
+  // Matx55d F;   // Matx55d predict_mat_;           /* 预测雅克比 */
+  // Matx35d H;   // Matx35d measurement_mat_;       /* 观测雅克比 */
+  // Matx55d P;   // Matx55d state_cov_;             /* 状态协方差 */
+  // Matx55d Q;   // Matx55d process_noi_cov_mat_;   /* 预测过程协方差 */
+  // Matx33d R;   // Matx33d measure_noi_cov_mat_;   /* 观测过程协方差 */
+  // Matx53d K;   // Matx35d kalman_gain_;           /* 卡尔曼增益 */
+  // Matx31d Yp;  // Matx31d predict_obs_;           /* 预测观测量 */
 
-  void InnerInit(const Vec5d& Xe);
+  EMatx51d EXe;
+  EMatx51d EXp;
+  EMatx55d EF;
+  EMatx35d EH;
+  EMatx55d EP;
+  EMatx55d EQ;
+  EMatx33d ER;
+  EMatx53d EK;
+  EMatx31d EYp;
+
+  double delta_t_;
+
+  void InnerInit(const Matx51d& Xe);
 
  public:
   EKF();
-  EKF(const Vec5d& Xe);
+  EKF(const Matx51d& Xe);
   ~EKF();
 
   void Init(const std::vector<double>& vec);
 
   const cv::Mat& Predict(const cv::Mat& measurements);
+  const cv::Mat& Update(const cv::Mat& upgrade);
 };
