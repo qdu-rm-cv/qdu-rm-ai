ba7205de58f86fb3493731cdd5af0fd7852613de
YangTisa <1480213331@qq.com>
2023-05-24 20:20:39 +0800

    未完善君瞄和弹道模型

--- src/apps/app/auto_aim/main.cpp
+++ src/apps/app/auto_aim/main.cpp
@@ -17,7 +17,7 @@
 #include "compensator.hpp"
 #include "hik_camera.hpp"
 #include "robot.hpp"
-
+// int counter = 0;
 class NumberClassifier {
  public:
   /**
@@ -57,8 +57,37 @@ class NumberClassifier {
       //     armor.right_light.top, armor.right_light.bottom};
       cv::Point2f lights_vertices[4];
       std::vector<cv::Point2f> image_vertices = armor.ImageVertices();
-      for (int i = 0; i < 4; i++) lights_vertices[i] = image_vertices[i];
-
+      for (int i = 0; i < 4; i++) {
+        lights_vertices[i] = image_vertices[i];
+      }
+      cv::Point2f left_light_bar_center =
+          (image_vertices[0] + image_vertices[1]) / 2;
+      cv::Point2f right_light_bar_center =
+          (image_vertices[3] + image_vertices[2]) / 2;
+      // cv::Point2f left_bar_top =
+      //     left_light_bar_center - (image_vertices[0] - image_vertices[1]) /
+      //     4;
+      // cv::Point2f left_bar_bottom =
+      //     left_light_bar_center + (image_vertices[0] - image_vertices[1]) /
+      //     4;
+      // cv::Point2f right_bar_top =
+      //     right_light_bar_center - (image_vertices[2] - image_vertices[3]) /
+      //     4;
+      // cv::Point2f right_bar_bottom =
+      //     right_light_bar_center + (image_vertices[2] - image_vertices[3]) /
+      //     4;
+      // lights_vertices[0] =
+      //     left_light_bar_center +
+      //     (image_vertices[0] - image_vertices[1]) / 4;  // left_bar_bottom;
+      // lights_vertices[1] =
+      //     left_light_bar_center -
+      //     (image_vertices[0] - image_vertices[1]) / 4;  // left_bar_top;
+      // lights_vertices[2] =
+      //     right_light_bar_center -
+      //     (image_vertices[2] - image_vertices[3]) / 4;  // right_bar_top;
+      // lights_vertices[3] =
+      //     right_light_bar_center +
+      //     (image_vertices[2] - image_vertices[3]) / 4;  // right_bar_bottom;
       const int top_light_y = (warp_height - light_length) / 2 - 1;
       const int bottom_light_y = top_light_y + light_length;
       const int warp_width =
@@ -69,39 +98,63 @@ class NumberClassifier {
           cv::Point(warp_width - 1, top_light_y),
           cv::Point(warp_width - 1, bottom_light_y),
       };
+      // cv::Mat a = src, clone();
+      // for (int i = 1; i <= 4; i++) {
+      //   cv::line(a, lights_vertices[(i - 1) % 4], lights_vertices[i % 4],
+      //            cv::Scalar(100, 100, 100));
+      // }
+
       cv::Mat number_image;
       auto rotation_matrix =
           cv::getPerspectiveTransform(lights_vertices, target_vertices);
       cv::warpPerspective(src, number_image, rotation_matrix,
                           cv::Size(warp_width, warp_height));
 
+      //进行腐蚀操作
+      // cv::Mat element =
+      //     cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 1));
+      // erode(src, number_image, element);
+
       // Get ROI
+      // cv::imshow("--------", number_image);
+
       number_image = number_image(
           cv::Rect(cv::Point((warp_width - roi_size.width) / 2, 0), roi_size));
+
+      // cv::imshow("----", number_image);
+      /*  cv::imread("/home/yang/Desktop/new/qdu-rm-ai/runtime/000455.png");*/
       // Binarize
       cv::cvtColor(number_image, number_image, cv::COLOR_RGB2GRAY);
       cv::threshold(number_image, number_image, 0, 255,
                     cv::THRESH_BINARY | cv::THRESH_OTSU);
-
       armor.number_img_ = number_image;
+      cv::imshow("-----------", number_image);
+      // std::string path;
+      // path = cv::format(
+      //     "/home/yang/Documents/rm_classifier_training-0.1/datasets/3/%d.png",
+      //     counter);
+      // switch (cv::waitKey(10)) {
+      //   case 's':
+      //     cv::imwrite(path, armor.number_img_);
+      //     counter++;
+      //     if (counter > 500) system("pause");
+      //     break;
+      //   case 'q':
+      //     break;
+      // }
     }
   }
 
   void classify(std::vector<Armor>& armors, const cv::Mat frame) {
     for (auto& armor : armors) {
-      cv::Mat
-          image =
-              cv::imread(
-                  "/home/yang/Desktop/rm_auto_aim/armor_detector/docs/"
-                  "num_bin.png");  // armor.number_img_.clone();
+      cv::Mat image = armor.number_img_.clone();
 
-      image.resize(20, 28);
       // Normalize
       image = image / 255.0;
-      cv::imshow("qq", image);
+
       // Create blob from image
       cv::Mat blob;
-      cv::dnn::blobFromImage(image, blob, 1., cv::Size(28, 20));
+      cv::dnn::blobFromImage(image, blob);
 
       // Set the input blob for the neural network
       net_.setInput(blob);
@@ -120,11 +173,10 @@ class NumberClassifier {
       cv::Point class_id_point;
       minMaxLoc(softmax_prob.reshape(1, 1), nullptr, &confidence, nullptr,
                 &class_id_point);
-      int label_id = class_id_point.x;
-
+      int label_id = class_id_point.x;  // id
       armor.confidence_ = confidence;
       armor.number_ = class_names_[label_id];
-      armor.armor_type_ = armor.GetArmorType();
+
       std::stringstream result_ss;
       result_ss << armor.number_ << ": " << std::fixed << std::setprecision(1)
                 << armor.confidence_ * 100.0 << "%";
@@ -132,34 +184,30 @@ class NumberClassifier {
     }
 
     armors.erase(
-        std::remove_if(
-            armors.begin(), armors.end(),
-            [this](const Armor& armor) {
-              SPDLOG_INFO(
-                  "armor.confidence_ :{} armor.number_{} armor.armor_type_:{}",
-                  armor.confidence_, armor.number_, armor.armor_type_);
-              if (armor.confidence_ < threshold ||
-                  armor.number_ == "Negative") {
-                return true;
-              }
+        std::remove_if(armors.begin(), armors.end(),
+                       [this](const Armor& armor) {
+                         SPDLOG_ERROR("------------" + armor.number_);
+                         if (armor.confidence_ < threshold) {
+                           return true;
+                         }
 
-              for (const auto& ignore_class : ignore_classes_) {
-                if (armor.number_ == ignore_class) {
-                  return true;
-                }
-              }
+                         for (const auto& ignore_class : ignore_classes_) {
+                           if (armor.number_ == ignore_class) {
+                             return true;
+                           }
+                         }
 
-              bool mismatch_armor_type = false;
-              if (armor.armor_type_ == LARGE) {
-                mismatch_armor_type = armor.number_ == "Outpost" ||
-                                      armor.number_ == "2" ||
-                                      armor.number_ == "Guard";
-              } else if (armor.armor_type_ == SMALL) {
-                mismatch_armor_type =
-                    armor.number_ == "1" || armor.number_ == "Base";
-              }
-              return mismatch_armor_type;
-            }),
+                         bool mismatch_armor_type = false;
+                         if (armor.armor_type_ == ArmorType::LARGE) {
+                           mismatch_armor_type = armor.number_ == "outpost" ||
+                                                 armor.number_ == "2" ||
+                                                 armor.number_ == "guard";
+                         } else if (armor.armor_type_ == ArmorType::SMALL) {
+                           mismatch_armor_type =
+                               armor.number_ == "1" || armor.number_ == "base";
+                         }
+                         return mismatch_armor_type;
+                       }),
         armors.end());
   }
 
@@ -180,7 +228,7 @@ class AutoAim : private App {
 
   component::Recorder recorder_ = component::Recorder("AutoAimThread");
   ArmorClassifier classifier_;
-
+  // kkk
  public:
   explicit AutoAim(const std::string& log_path)
       : App(log_path), detector_async_(ArmorDetectorAsync(2)) {
@@ -192,9 +240,9 @@ class AutoAim : private App {
     cam_.Setup(kIMAGE_WIDTH, kIMAGE_HEIGHT);
     detector_async_.LoadParams(kPATH_RUNTIME + "RMUL2022_Armor.json");
     compensator_.LoadCameraMat(kPATH_RUNTIME + "MV-CA016-10UC-6mm_1.json");
-    classifier_.LoadModel(kPATH_RUNTIME + "armor_classifier.onnx");
-    classifier_.LoadLable(kPATH_RUNTIME + "armor_classifier_lable.json");
-    classifier_.SetInputSize(cv::Size(28, 28));
+    // classifier_.LoadModel(kPATH_RUNTIME + "mpl.onnx");
+    // classifier_.LoadLable(kPATH_RUNTIME + "label.json");
+    // classifier_.SetInputSize(cv::Size(28, 28));
 
     // do {
     //   std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -216,11 +264,11 @@ class AutoAim : private App {
     cv::Mat frame;
     tbb::concurrent_vector<Armor> armors;
     detector_async_.Start();
-    std::string path1 = kPATH_RUNTIME + "fc.onnx";
+    std::string path1 = kPATH_RUNTIME + "mlp.onnx";
     std::string path2 = kPATH_RUNTIME + "label.txt";
-    // std::vector<std::string> mask = {"Base", "1", "2",     "3",
-    //                                  "4",    "5", "Guard", "Outpost"};
-    NumberClassifier num_classfier(path1, path2, 0.3);
+    std::vector<std::string> mask = {"Negative"};
+    NumberClassifier num_classfier(
+        "/home/yang/Desktop/new/qdu-rm-ai/runtime/mlp.onnx", path2, 0.8);
     while (1) {
       if (!cam_.GetFrame(frame)) continue;
 
@@ -234,29 +282,31 @@ class AutoAim : private App {
       for (auto armor : armors) {
         armors_t.push_back(armor);
       }
+      // cv::imshow("face", armors_t.front().number_img_);
+      SPDLOG_ERROR("11111111111111111111111");
       SPDLOG_ERROR("armor.num.1 {}", armors_t.size());
       num_classfier.extractNumbers(frame, armors_t);
       num_classfier.classify(armors_t, frame);
-      SPDLOG_ERROR("11111111111111111111111");
-      // cv::imshow("face", armors_t.front().number_img_);
+
       tbb::concurrent_vector<Armor> armors2;
       for (auto armor : armors_t) {
         armors2.push_back(armor);
       }
       SPDLOG_ERROR("armor.num {}", armors2.size());
       if (!armors2.empty())
-        SPDLOG_ERROR("classfier {}");
+        SPDLOG_ERROR("classfier {}");  // pai xv xou hua
       else {
         SPDLOG_ERROR("EMPTY!!!");
+        continue;
       }
-      compensator_.Apply(armors, /*robot_.GetBalletSpeed()*/ 999,
-                         component::Euler(0, 0, 0) /*robot_.GetEuler()*/,
+      compensator_.Apply(armors2, /*robot_.GetBalletSpeed()*/ 999,
+                         component::Euler(0, 0, 0) /* robot_.GetEuler()*/,
                          game::AimMethod::kARMOR);
       // manager_.Aim(armors.front().GetAimEuler());
       // robot_.Pack(manager_.GetData(), 9999);
 
       SPDLOG_WARN("pack");
-      recorder_.Record();
+      // recorder_.Record();
     }
   }
 };
